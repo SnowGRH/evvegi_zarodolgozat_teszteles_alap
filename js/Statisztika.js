1beada41ecca4c8598997c13769a451ec59898ab
cskati <csefi.katalin@gmail.com>
2023-05-31 12:18:49 +0200

    alap

---
+++ js/Statisztika.js
@@ -0,0 +1,37 @@
+class Statisztika {
+    #lista;
+    constructor(lista) {
+        this.#lista = lista;
+    }
+    orszagokSzerintAutokSzama(adat) {
+        let db = 0;
+        this.#lista.forEach((element) => {
+            if (element.orszag === adat) {
+                db++;
+            }
+        });
+        return db;
+    }
+    legregebbiAuto() {
+        let minHely = 0;
+        let minErtek=this.#lista[minHely]
+        this.#lista.forEach((element,index) => {
+            if(element<minErtek){
+                 minHely = index;
+                 minErtek=this.#lista[minHely]
+            }
+        });
+        return minHely
+    }
+    orszagokSzerintLegregebbiAuto(adat) {
+        let minHely = 0;
+        let minErtek=this.#lista[minHely]
+        this.#lista.forEach((element,index) => {
+            if(element.orszag===adat && element<minErtek){
+                 minHely = index;
+                 minErtek=this.#lista[minHely]
+            }
+        });
+        return minHely
+    }
+}
